01e1bdf89f37473df9883255869af9b17f3eb191
Vikas Lamba <vikas13jun@gmail.com>
2022-11-22 19:07:32 +0100

    Updated start script

--- src/iot_server/settings.py
+++ src/iot_server/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i&uv2ho(x=lzk#3qwc4^!-ut$67h=h5o(#)&pgnqsrbd3$uj3t'
+SECRET_KEY = os.environ.get("SECRET_KEY", "___CHANGE____ME___")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = [
     "*"
--- src/start-services.sh
+++ src/start-services.sh
@@ -1,2 +1,3 @@
+python manage.py migrate
 python manage.py runserver 0:8000
 tail -f /dev/null
