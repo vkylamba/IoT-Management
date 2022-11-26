6ab4116c4b9f86881d6789da3fd984b76e28a0d9
vkylamba <vikas13jun@gmail.com>
2022-11-26 19:30:10 +0100

    updated start script

--- src/start-services.sh
+++ src/start-services.sh
@@ -1,4 +1,6 @@
 python manage.py migrate
 python manage.py createsuperuser --email=${DJANGO_SUPERUSER_EMAIL} --noinput
-python manage.py runserver 0:8000
+python manage.py collectstatic --no-input
+# python manage.py runserver 0:8000
+daphne -b 0.0.0.0 -p 8000
 tail -f /dev/null
