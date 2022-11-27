d46cc846eee61def11305eff61b2c644ca5ce698
vkylamba <vikas13jun@gmail.com>
2022-11-27 13:14:21 +0100

    time fix

--- src/dashboard/data_scripts/data_update_signal_receiver.py
+++ src/dashboard/data_scripts/data_update_signal_receiver.py
@@ -3,9 +3,11 @@ import json
 import logging
 from datetime import datetime, timedelta
 
-from device.models import DeviceStatus
+import pytz
 from django.conf import settings
 from django.core.cache import cache
+
+from device.models import DeviceStatus
 
 from .property_updaters import update_device_properties
 from .widgets_updater import update_user_widgets
@@ -15,7 +17,7 @@ logger = logging.getLogger("application")
 
 def update_device_info_on_meter_data_update(device, meters_and_data, data_arrival_time=None):
     other_data = device.other_data if device.other_data is not None else {}
-    last_update_time = other_data.get('properties_update_time')
+    last_update_time = other_data.get('properties_update_time').astimezone(pytz.utc)
     this_data_time = data_arrival_time if data_arrival_time is not None else datetime.utcnow()
     data_updated = False
 
@@ -24,7 +26,7 @@ def update_device_info_on_meter_data_update(device, meters_and_data, data_arriva
         other_data['properties_update_time'] = this_data_time.strftime(settings.TIME_FORMAT_STRING)
         data_updated = True
     else:
-        last_update_time = datetime.strptime(last_update_time, settings.TIME_FORMAT_STRING)
+        last_update_time = datetime.strptime(last_update_time, settings.TIME_FORMAT_STRING).astimezone(pytz.utc)
         if (this_data_time - last_update_time) > timedelta(minutes=settings.DEVICE_PROPERTY_UPDATE_DELAY_MINUTES):
             data = update_device_properties(device, meters_and_data)
             update_user_widgets(device)
