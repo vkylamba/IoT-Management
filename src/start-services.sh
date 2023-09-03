e693db48519c52e6662a888ef86f91e016f85111
Vikas Lamba <vikas13jun@gmail.com>
2023-09-03 09:01:35 +0200

    Fix script

--- src/start-services.sh
+++ src/start-services.sh
@@ -1,8 +1,9 @@
+!/bin/bash
 echo -e $ROOT_CA_CERT > root_ca.crt
 python manage.py migrate
 python manage.py createsuperuser --email=${DJANGO_SUPERUSER_EMAIL} --noinput
 python manage.py collectstatic --no-input
 python manage.py startbot &
 python manage.py mqtt &
-daphne -b 0.0.0.0 -p 8000 iot_server.asgi:application
+python -m daphne -b 0.0.0.0 -p 8000 iot_server.asgi:application
 tail -f /dev/null
