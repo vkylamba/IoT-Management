1ba3dcb0dc92d42881e05abcaa5fd10c1effbd21
Vikas Lamba <vikas13jun@gmail.com>
2023-09-15 08:58:10 +0200

    Updated user apis, add/edit user device/status types

--- src/api/viewsets/user_views.py
+++ src/api/viewsets/user_views.py
@@ -3,9 +3,11 @@ import logging
 
 from api.permissions import IsDeviceUser
 from api.serializers import UserSerializer
+from device.models import StatusType, UserDeviceType
 from django.conf import settings
 from django.contrib.auth import get_user_model
-from rest_framework import filters, viewsets
+from django.db.models import Q
+from rest_framework import filters, status, viewsets
 from rest_framework.parsers import FileUploadParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -41,4 +43,66 @@ class UserViewSet(viewsets.ModelViewSet):
             json_doc = request.data.pop('document')[0]
             data = json.loads(json_doc.read())
             request.data.update(data)
+
+        if request.user.has_permission(PERMISSIONS_ADMIN):
+            # handle the device and status types update
+            available_device_types = request.data.get("available_device_types", [])
+            available_status_types = request.data.get("available_status_types", [])
+
+            errors = []
+            user_device_types = UserDeviceType.objects.filter(user=request.user)
+            devices_ids_to_keep = []
+            for available_device_type in available_device_types:
+                device_type_id = available_device_type.get("id")
+                if device_type_id is not None:
+                    device_type = user_device_types.filter(
+                        id=device_type_id
+                    ).first()
+                    if device_type is None:
+                        errors.append(f"Device type with id {device_type_id} not found!")
+                        continue
+                else:
+                    device_type = UserDeviceType()
+                if len(errors) == 0:
+                    device_type.name = available_device_type.get("name", device_type.code)
+                    device_type.code = device_type.name.upper() if device_type.name is not None else None
+                    device_type.details = available_device_type.get("details", device_type.details)
+                    device_type.identifier_field = available_device_type.get("identifier_field", device_type.identifier_field)
+                    device_type.data_schema = available_device_type.get("data_schema", device_type.data_schema)
+                    device_type.user = request.user
+                    device_type.save()
+                devices_ids_to_keep.append(device_type.id)
+            
+            # Delete the remaining device types
+            user_device_types.filter(~Q(id__in=devices_ids_to_keep)).delete()
+
+            user_status_types = StatusType.objects.filter(user=request.user)
+            status_ids_to_keep = []
+            for available_status_type in available_status_types:
+                status_type_id = available_status_type.get("id")
+                if status_type_id is not None:
+                    status_type = StatusType.objects.filter(
+                        id=status_type_id
+                    ).first()
+                    if status_type is None:
+                        errors.append(f"Status type with id {status_type_id} not found!")
+                        continue
+                else:
+                    status_type = StatusType()
+                if len(errors) == 0:
+                    status_type.name = available_status_type.get("name", status_type.name)
+                    status_type.target_type = available_status_type.get("target_type", status_type.target_type)
+                    status_type.update_trigger = available_status_type.get("update_trigger", status_type.update_trigger)
+                    status_type.user = request.user
+                    status_type.translation_schema = available_status_type.get("translation_schema", status_type.translation_schema)
+                    status_type.save()
+                
+                status_ids_to_keep.append(status_type.id)
+            
+            # Delete the remaining status types
+            user_status_types.filter(~Q(id__in=status_ids_to_keep)).delete()
+            
+            if len(errors) > 0:
+                return Response(errors, status=status.HTTP_400_BAD_REQUEST)
+
         return super(UserViewSet, self).partial_update(request, pk)
--- src/device/models/status.py
+++ src/device/models/status.py
@@ -1,6 +1,7 @@
 import uuid
 
 from django.db import models
+from django_celery_beat.models import CrontabSchedule
 
 
 class UserDeviceType(models.Model):
@@ -36,16 +37,42 @@ class UserDeviceType(models.Model):
         return f"{self.code}-{self.user}"
 
 
+STATUS_TARGET_TYPES = (
+    ('STATUS_TARGET_METER', 'meter'),
+    ('STATUS_TARGET_USER', 'user'),
+    ('STATUS_TARGET_ALARM', 'alarm'),
+    ('STATUS_TARGET_DEVICE', 'device'),
+    ('STATUS_TARGET_REPORT', 'report'),
+)
+
+STATUS_UPDATE_TRIGGER = (
+    ('data', 'Data'),
+    ('schedule', 'Schedule'),
+    ('data/schedule', 'Data and Schedule'),
+)
 class StatusType(models.Model):
     """
         model to store status types.
     """
+    STATUS_TARGET_METER = 'meter'
+    STATUS_TARGET_USER = 'user'
+    STATUS_TARGET_ALARM = 'alarm'
+    STATUS_TARGET_DEVICE = 'device'
+    STATUS_TARGET_REPORT = 'report'
+
+    STATUS_UPDATE_TRIGGER_DATA = 'data'
+    STATUS_UPDATE_TRIGGER_SCHEDULE = 'schedule'
+    STATUS_UPDATE_TRIGGER_ANY = 'any'
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255, help_text='Type name')
-    code = models.CharField(max_length=255, help_text='Type code')
     user = models.ForeignKey('User', blank=True, null=True, on_delete=models.DO_NOTHING)
     device = models.ForeignKey('Device', blank=True, null=True, on_delete=models.DO_NOTHING)
     device_type = models.ForeignKey('UserDeviceType', blank=True, null=True, on_delete=models.DO_NOTHING)
+    target_type = models.CharField(max_length=255, choices=STATUS_TARGET_TYPES, help_text='Resulting status type')
+    update_trigger = models.CharField(max_length=255, choices=STATUS_UPDATE_TRIGGER, help_text='Status update trigger')
+    schedule = models.ForeignKey(CrontabSchedule, null=True, blank=True, on_delete=models.CASCADE)
+    last_trigger_time = models.DateTimeField(null=True, blank=True)
     translation_schema = models.JSONField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=True)
