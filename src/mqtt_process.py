b52817102ec6108dac642ee8fe39b5ca40c9b8a2
Vikas Lamba <vikas13jun@gmail.com>
2024-07-17 16:06:42 +0200

    Fix mqtt process

--- src/mqtt_process.py
+++ src/mqtt_process.py
@@ -1,8 +1,15 @@
 import os
 import time
-
+import subprocess
 
 while True:
     print("Starting MQTT process...")
-    os.system("python manage.py mqtt")
+    # Check if the process is already running
+    proc = subprocess.Popen(["ps -ef | grep 'python manage.py mqtt'"], stdout=subprocess.PIPE, shell=True)
+    (out, err) = proc.communicate()
+    processes_running = out.decode().split('\n')
+    if processes_running > 2:
+        print("MQTT process already running.")
+    else:
+        os.system("python manage.py mqtt")
     time.sleep(10)
